64ae0dae05770c73e2372596d303e77c1f6b2e5f
safds <958488568@qq.com>
2017-05-11 18:54:59 +0800

    Signed-off-by: safds <958488568@qq.com>

---
+++ js/tubiao.js
@@ -0,0 +1,60 @@
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: ''
+        },
+        xAxis: {
+            categories: ['HTML5+CSS3', 'JAVASCRIPT', 'jQuery', 'Bootstrap', 'Angular']
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: ''
+            }
+        },
+        legend: {
+            reversed: true
+        },
+        plotOptions: {
+            series: {
+                stacking: 'normal'
+            }
+        },
+        series: [{
+            name: 'Master',
+            data: [90, 80, 80, 85, 85]
+        }]
+    });
+    $('#container2').highcharts({
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: ''
+        },
+        xAxis: {
+            categories: ['Photoshop', 'Illustrator']
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: ''
+            }
+        },
+        legend: {
+            reversed: true
+        },
+        plotOptions: {
+            series: {
+                stacking: 'normal'
+            }
+        },
+        series: [{
+            name: 'Master',
+            data: [90, 50]
+        }]
+    });
+});
